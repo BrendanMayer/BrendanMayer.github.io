fd434ef46b202c08f53e887e79cbca6ea08bcca7
BrendanMayer <brendan.mayer1234@gmail.com>
2025-10-08 20:59:41 +0100

    Fix Astro config and GH Pages workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,10 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import mdx from '@astrojs/mdx';
+// astro.config.mjs
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import mdx from '@astrojs/mdx'
 
 export default defineConfig({
-  site: 'https://brendanmayer.github.io/',
+  site: 'https://brendanmayer.github.io',
   integrations: [tailwind(), mdx()],
-  markdown: {
-    shikiConfig: {
-      theme: 'nord'
-    }
-  }
-});
+  markdown: { shikiConfig: { theme: 'nord' } }
+})
